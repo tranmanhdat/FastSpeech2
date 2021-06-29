922f7bd35e0474745a5c6ce17b96add6d663a8fb
phamlehuy53 <unknownsol98@gmail.com>
2021-06-29 15:32:13 +0700

    new lexicon

--- text/cleaners.py
+++ text/cleaners.py
@@ -15,7 +15,8 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
 
 # Regular expression matching whitespace:
 import re
-from string import punctuation
+# from string import punctuation
+punctuation = "!\"#$%&'()*+-/:;<=>?@[\]^_`{|}~"
 from unidecode import unidecode
 from .numbers import normalize_numbers
 _whitespace_re = re.compile(r'\s+')
@@ -40,6 +41,8 @@ _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in 
     ('ltd', 'limited'),
     ('col', 'colonel'),
     ('ft', 'fort'),
+    ('j', 'gi'),
+    ('tnhh', 'trách nhiệm hữu hạn')
 ]]
 
 
@@ -90,12 +93,12 @@ def english_cleaners(text):
     return text
 
 def vietnamese_cleaners(text):
-    text = re.sub('['+punctuation+']', ',', text) # 
+    text = re.sub('['+punctuation+']', ' ', text) # 
     text = lowercase(text)
-    text = expand_numbers(text)
+    # text = expand_numbers(text) # TODO: expand vietnamese numbers
 
     # TODO:Need abbreviation for vietnamese
-    # text = expand_abbreviations(text) 
+    text = expand_abbreviations(text) 
     text = collapse_whitespace(text)
     text = text.strip()
     return text
