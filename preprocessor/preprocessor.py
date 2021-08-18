48806e1bed974f488b962921ddc8682ba8312bed
phamlehuy53 <phamlehuy53@gmail.com>
2021-08-18 07:51:54 +0700

    update vie

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -84,10 +84,10 @@ class Preprocessor:
                         info, pitch, energy, n = ret
                     out.append(info)
 
-                # if 'pitch' in locals() and len(pitch) > 0:
-                    pitch_scaler.partial_fit(pitch.reshape((-1, 1)))
-                # if 'energy' in locals() and len(energy) > 0:
-                    energy_scaler.partial_fit(energy.reshape((-1, 1)))
+                    if len(pitch) > 0:
+                        pitch_scaler.partial_fit(pitch.reshape((-1, 1)))
+                    if len(energy) > 0:
+                        energy_scaler.partial_fit(energy.reshape((-1, 1)))
 
                 # if 'n' in locals():
                     n_frames += n
